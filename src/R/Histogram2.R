36c8d6b9547df04478575c2de5c3a90991389a70
rob-77 <rob.77@live.com>
2026-02-13 21:19:55 -0600

    Update Histogram2.R

--- src/R/Histogram2.R
+++ src/R/Histogram2.R
@@ -3,3 +3,4 @@ print(c(mean(x), var(x), sd(x)))
 # 51.350000 39.818421  6.310184
 h = hist(x, breaks = c(40, 45, 50, 55, 60, 65), include.lowest = TRUE, right = FALSE)
 # h = hist(x, breaks = seq(40, 65, 5), include.lowest = TRUE, right = FALSE)
+
